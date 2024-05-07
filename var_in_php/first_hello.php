034fedb82b184ef9c7ac06e10b7815c878b8f4ae
Tan Hoa <letanhoa2003@gmail.com>
2024-05-07 22:30:49 +0700

    learn about variables in PHP

---
+++ var_in_php/first_hello.php
@@ -0,0 +1,10 @@
+<?php
+// một biến trong PHP là tên của vị trí bộ nhớ chứa dữ liệu 
+$_hoale = 'hoale';
+$bien_so_1 = 10;
+
+$bien_sO_1 = 20; //phân biệt hoa và thường
+
+echo 'le tan hoa';
+echo $bien_so_1;
+echo $bien_sO_1;
